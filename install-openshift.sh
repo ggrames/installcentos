3330e13f06f92f814feebd767a446e3463df6e88
Grames Gernot <gernot.grames@gmx.at>
2018-04-12 14:37:22 +0200

    Added Proxy Usage

--- install-openshift.sh
+++ install-openshift.sh
@@ -6,8 +6,9 @@ export DOMAIN=${DOMAIN:="$(curl ipinfo.io/ip).nip.io"}
 export USERNAME=${USERNAME:="$(whoami)"}
 export PASSWORD=${PASSWORD:=password}
 export VERSION=${VERSION:="v3.7.1"}
+export $TPROXY=${TPROXY:=""}
 
-export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/ggrames/installcentos/master"}
+export SCRIPT_REPO=${SCRIPT_REPO:="https://raw.githubusercontent.com/gshipley/installcentos/master"}
 
 export IP="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
 export API_PORT=${API_PORT:="8443"}
@@ -21,55 +22,68 @@ echo "******"
 
 yum install -y epel-release
 
-yum install -y git wget zile nano net-tools docker \
+export http_proxy=$TPROXY
+export https_proxy=$TPROXY
+
+echo "proxy=$TPROXY" >> /etc/yum.conf
+
+yum update -y
+yum install -y git wget zile nano net-tools docker-1.12.6 \
 python-cryptography pyOpenSSL.x86_64 python2-pip \
 openssl-devel python-devel httpd-tools NetworkManager python-passlib \
 java-1.8.0-openjdk-headless "@Development Tools"
 
-git config --global http.proxy http_proxy=$TProxy
-git config --global https.proxy https_proxy=$TProxy
-systemctl | grep "NetworkManager.*running" 
+systemctl | grep "NetworkManager.*running"
 if [ $? -eq 1 ]; then
-	systemctl start NetworkManager
-	systemctl enable NetworkManager
+        systemctl start NetworkManager
+        systemctl enable NetworkManager
 fi
 
 which ansible || pip install -Iv ansible
 
-[ ! -d openshift-ansible ] && git clone https://github.com/ggrames/openshift-ansible.git
+[ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git
 
-cd openshift-ansible && git fetch && git checkout release-3.7-hotfix && cd ..
+cd openshift-ansible && git fetch && git checkout release-3.7 && cd ..
 
 cat <<EOD > /etc/hosts
-127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 
+127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-${IP}		$(hostname) console console.${DOMAIN} 
+${IP}           $(hostname) console console.${DOMAIN}
 EOD
 
-if [ -z $DISK ]; then 
-	echo "Not setting the Docker storage."
+if [ -z $DISK ]; then
+        echo "Not setting the Docker storage."
 else
-	cp /etc/sysconfig/docker-storage-setup /etc/sysconfig/docker-storage-setup.bk
+        cp /etc/sysconfig/docker-storage-setup /etc/sysconfig/docker-storage-setup.bk
 
-	echo DEVS=$DISK > /etc/sysconfig/docker-storage-setup
-	echo VG=DOCKER >> /etc/sysconfig/docker-storage-setup
-	echo SETUP_LVM_THIN_POOL=yes >> /etc/sysconfig/docker-storage-setup
-	echo DATA_SIZE="100%FREE" >> /etc/sysconfig/docker-storage-setup
+        echo DEVS=$DISK > /etc/sysconfig/docker-storage-setup
+        echo VG=DOCKER >> /etc/sysconfig/docker-storage-setup
+        echo SETUP_LVM_THIN_POOL=yes >> /etc/sysconfig/docker-storage-setup
+        echo DATA_SIZE="100%FREE" >> /etc/sysconfig/docker-storage-setup
 
-	systemctl stop docker
+        systemctl stop docker
 
-	rm -rf /var/lib/docker
-	wipefs --all $DISK
-	docker-storage-setup
+        rm -rf /var/lib/docker
+        wipefs --all $DISK
+        docker-storage-setup
 fi
+
+mkdir -p /etc/systemd/system/docker.service.d/
+
+echo "[Service]
+Environment=\"HTTP_PROXY=$TPROXY\"" > /etc/systemd/system/docker.service.d/http-proxy.conf
+echo "[Service]
+Environment=\"HTTPS_PROXY=$TPROXY\"" > /etc/systemd/system/docker.service.d/https-proxy.conf
+
+systemctl daemon-reload
 
 systemctl restart docker
 systemctl enable docker
 
 if [ ! -f ~/.ssh/id_rsa ]; then
-	ssh-keygen -q -f ~/.ssh/id_rsa -N ""
-	cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-	ssh -o StrictHostKeyChecking=no root@$IP "pwd" < /dev/null
+        ssh-keygen -q -f ~/.ssh/id_rsa -N ""
+        cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+        ssh -o StrictHostKeyChecking=no root@$IP "pwd" < /dev/null
 fi
 
 export METRICS="True"
@@ -78,14 +92,14 @@ export LOGGING="True"
 memory=$(cat /proc/meminfo | grep MemTotal | sed "s/MemTotal:[ ]*\([0-9]*\) kB/\1/")
 
 if [ "$memory" -lt "4194304" ]; then
-	export METRICS="False"
+        export METRICS="False"
 fi
 
 if [ "$memory" -lt "8388608" ]; then
-	export LOGGING="False"
+        export LOGGING="False"
 fi
 
-curl -o inventory.download -x $TProxy $SCRIPT_REPO/inventory.ini
+curl -o inventory.download $SCRIPT_REPO/inventory.ini
 envsubst < inventory.download > inventory.ini
 ansible-playbook -i inventory.ini openshift-ansible/playbooks/byo/config.yml
 
@@ -93,6 +107,9 @@ htpasswd -b /etc/origin/master/htpasswd ${USERNAME} ${PASSWORD}
 oc adm policy add-cluster-role-to-user cluster-admin ${USERNAME}
 
 systemctl restart origin-master-api
+
+unset http_proxy
+unset https_proxy
 
 echo "******"
 
