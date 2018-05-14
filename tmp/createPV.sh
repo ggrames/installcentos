845e38ef61843042a124f3a1da226fe5b95cdf1e
Grames Gernot <gernot.grames@gmx.at>
2018-05-14 12:29:56 +0200

    Update createPV.sh

--- tmp/createPV.sh
+++ tmp/createPV.sh
@@ -2,13 +2,15 @@
 
 for i in `seq 1 10`;
 do
-	DIRNAME="vol$i"
-	mkdir -p /mnt/data/$DIRNAME
+        DIRNAME="vol$i"
+        mkdir -p /mnt/data/$DIRNAME
         chcon -Rt svirt_sandbox_file_t /mnt/data/$DIRNAME
-	chmod 777 /mnt/data/$DIRNAME
-	sleep 5
-	sed -i "s/name: vol/name: vol$i/g" vol.yaml
-	sed -i "s/path: \/mnt\/data\/vol/path: \/mnt\/data\/vol$i/g" vol.yaml
-	oc create -f vol.yaml
-	echo "created volume $i"
+        chmod 777 /mnt/data/$DIRNAME
+        sleep 5
+        cp vol.yaml vol_temp.yaml
+        sed -i "s/name: vol/name: vol$i/g" vol_temp.yaml
+        sed -i "s/path: \/mnt\/data\/vol/path: \/mnt\/data\/vol$i/g" vol_temp.yaml
+        oc create -f vol_temp.yaml
+        rm -f vol_temp.yaml
+        echo "created volume $i"
 done
